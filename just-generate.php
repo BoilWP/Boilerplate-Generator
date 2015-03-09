a501f1e097d9689d72398b96f542d96e6ece6ed0
Gennady Kovshenin <gennady@kovshenin.com>
2015-03-09 18:59:04 +0500

    More replacements
    Filenames, identifiers, etc.

--- just-generate.php
+++ just-generate.php
@@ -30,6 +30,8 @@ function do_replacements( $contents, $filename, $your_plugin, $prototype ) {
 		foreach ( $plugin_headers as $key => $value ) {
 			$contents = preg_replace( '/(' . preg_quote( $key ) . ':)\s?(.+)/', '\\1 ' . $value, $contents );
 		}
+
+		$contents = preg_replace( '/(public $web_url =).*/', "\\1 '" . $your_plugin['uri'] . "';", $contents ); // Plugin slug
 	} else if ( $filename == 'license.txt' ) {
 		$contents = preg_replace( '/Copyright (\d\d\d\d) by the contributors/', 'Copyright ' . date( 'Y' ) . ' by ' . $your_plugin['author'], $contents );
 	}
@@ -38,14 +40,25 @@ function do_replacements( $contents, $filename, $your_plugin, $prototype ) {
 	// Function names can not contain hyphens.
 	$slug = str_replace( '-', '_', $your_plugin['slug'] );
 
-	// Regular treatment for all other files.
 	$contents = str_replace( $prototype['fullname'], $your_plugin['name'], $contents ); // Generic names in licenses, etc.
+
 	$contents = str_replace( 'plugin_name_', $slug . '_', $contents ); // Function names.
+	$contents = str_replace( '_plugin_name', '_' . $slug, $contents ); // Function names.
+
+	$contents = str_replace( 'plugin_name', $your_plugin['slug'], $contents ); // Miscellaneous strings and identifiers
+	$contents = str_replace( 'plugin-name', $your_plugin['slug'], $contents ); // Filename identifiers
+
+	$contents = str_replace( 'PLUGIN_NAME', strtoupper( $slug ), $contents ); // Definition names.
+
 	$contents = str_replace( 'Plugin_Name', implode( '_', array_map( 'ucfirst', explode( '-', $your_plugin['slug'] ) ) ), $contents ); // Classes, etc.
+
 	$contents = str_replace( "'" . $prototype['name'] . "'", "'" . $your_plugin['slug'] . "'", $contents ); // Strings
 	$contents = str_replace( "\\'" . $prototype['name'] . "\\'", "\\'" . $your_plugin['slug'] . "\\'", $contents ); // Strings
 	$contents = str_replace( '"' . $prototype['name'] . '"', '"' . $your_plugin['slug'] . '"', $contents ); // Strings
 	$contents = str_replace( '\"' . $prototype['name'] . '\"', '\"' . $your_plugin['slug'] . '\"', $contents ); // Strings
+
+	$contents = preg_replace( '/.*@todo.*/', '', $contents ); // Remove @todo statements
+	$contents = preg_replace( '/(.*@author).*/', sprintf( '\\1 %s', $your_plugin['author'] ), $contents ); // Change package authorship
 
 	return $contents;
 }
@@ -147,13 +160,10 @@ function _init() {
 		// File content replacements
 		$contents = file_get_contents( $filename );
 		$contents = do_replacements( $contents, $local_filename, $your_plugin, $prototype );
-		if ( $local_filename == 'wordpress-plugin-boilerplate.php' ) {
-			echo $local_filename;
-			echo '<pre>'; echo esc_html( var_export( $contents, true ) ); die();
-		}
 
 		// Filename replacements, assuming that the prototype plugin is called the same as its directory
 		$local_filename = str_replace( $prototype['name'], $your_plugin['slug'], $local_filename );
+		$local_filename = str_replace( 'plugin-name', $your_plugin['slug'], $local_filename );
 
 		$zip->addFromString( trailingslashit( $your_plugin['slug'] ) . $local_filename, $contents );
 	}
